4be08f950f818236b3a82c3d5935e0aac188d650
Samarth Vohra <officialdevelopersamarth@gmail.com>
2024-04-05 21:41:37 +0530

    lecture 12 codes

---
+++ Lecture-12/js-intro/app.js
@@ -0,0 +1,40 @@
+// let naam = "samarth vohra";
+// let age = 29;
+// let city = "delhi";
+
+// // hi samarth vohra this side and i am 29 y/o and my city is delhi.
+
+// let out =
+//   "hi " + naam + " this side and i am " + age + " y/o and my city is " + city;
+// console.log(out);
+
+// // -----------
+
+// // string template literals (3rd -> backticks)
+// let out2 = "hi ${naam} this side and i am ${age} y/o and my city is ${city}";
+// let out3 = `hi ${naam} this side and i am ${age} y/o and my city is ${city}`;
+
+// console.log(out2);
+// console.log(out3);
+
+// ----------------
+
+//redeclare 🚫
+// let str = "sam";
+// let str = 10;
+
+//reassign ✅
+// let abc = 10;
+// abc = "sam";
+
+// -----------
+
+// let a = true;
+// let b = false;
+// console.log(a, b);
+
+let score = 100;
+
+let out = score == 10;
+let out2 = score == 100;
+console.log(out, out2);
