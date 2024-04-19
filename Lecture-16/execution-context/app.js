6cab7504143033d4ebb58a754ce55f467a820ca7
Samarth Vohra <officialdevelopersamarth@gmail.com>
2024-04-19 21:26:06 +0530

    lecture 16 codes

---
+++ Lecture-16/execution-context/app.js
@@ -0,0 +1,24 @@
+// var username = "samarth";
+
+// function sam() {
+//   console.log("hi i am sam");
+// }
+
+// console.log(username);
+// sam();
+
+// -------------------
+
+// var username = "samarth";
+
+// function sam() {
+//   var a = 10;
+//   var b = 20;
+
+//   console.log("hi i am sam");
+//   return a + b;
+// }
+
+// console.log(username);
+// let output = sam();
+// console.log(output);
---
+++ Lecture-16/hoisting/app.js
@@ -0,0 +1,27 @@
+// var a = 10;
+// function sam() {
+//   console.log("hello");
+// }
+
+// console.log(a);
+// sam();
+
+// ----------
+
+// console.log(a);
+// sam();
+
+// var a = 10;
+// function sam() {
+//   console.log("hello");
+// }
+
+// ------------
+
+console.log(a);
+sam();
+
+let a = 10;
+function sam() {
+  console.log("hello");
+}
